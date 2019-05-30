68c9a711f0b5cbe4c6c973889cae02a22daa24e5
Iby <Eryanobe@me.com>
2019-05-30 00:31:21 -0700

    updated files

--- liri.js
+++ liri.js
@@ -6,11 +6,9 @@ var request = require('request');
 var keys = require('./keys.js');
 var axios = require("axios");
 var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 var moment = require('moment');
 
-fs.appendFile('log.txt', command + ",", function (err) {
-    if (err) throw err;
-});
 
 var command = process.argv[2];
 var input = process.argv[3];
@@ -33,15 +31,56 @@ switch (command) {
 }
 
 function searchForBandsInTown(artist) {
-    // var artist = value;
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-        .then(function (response) {
-            console.log("Name of the venue:", response.data[0].venue.name);
-            console.log("Venue location:", response.data[0].venue.city);
-            var eventDate = moment(response.data[0].datetime).format('MM/DD/YYYY');
-            console.log("Date of the Event:", eventDate);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+
+    var url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    axios.get(url).then(
+        function (response) {
+            // console.log(response.data)
+            for (var i = 0; i < response.data.length; i++) {
+                console.log("Concert Time: " + moment(response.data[i].datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYYY, h:mm A'));
+                console.log("Concert Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
+                console.log("Concert Venue: " + response.data[i].venue.name);
+                console.log('--------------------------------------------------')
+                fs.appendFileSync('log.txt', "\r\n" + "Concert Search Log----------------------" + "\r\n", 'utf8');
+                fs.appendFileSync('log.txt', "\r\n" + "Venue Name: " + response.data[i].venue.name + "\r\n", 'utf8');
+                fs.appendFileSync('log.txt', "\r\n" + "Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country + "\r\n", 'utf8');
+                fs.appendFileSync('log.txt', "\r\n" + "Venue Time: " + moment(response.data[i].datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYYY, h:mm A') + "\r\n", 'utf8');
+                fs.appendFileSync('log.txt', "\r\n" + "-----------------------------------------" + "\r\n", 'utf8');
+            }
+        }
+    );
+
+};
+
+function spotifyThisSong(song) {
+
+    if (!song) {
+        song = "the sign Ace of Base"
+    }
+    spotify.search({
+        type: 'track',
+        query: song
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        //   console.log(data.tracks.items[0]); 
+        console.log("\n---------------------\nSong Name: " + data.tracks.items[0].name);
+        console.log("Artist(s) Name: " + data.tracks.items[0].artists[0].name);
+        console.log("Album Name: " + data.tracks.items[0].album.name);
+        //   data.tracks.items[0].artists.forEach(artist => {
+        //       console.log(artist.name)
+        //   })
+        console.log("Preview URL: " + data.tracks.items[0].preview_url + "\n---------------\n");
+
+        //adds text to log.txt
+        fs.appendFileSync('log.txt', "\r\n" + "Song Search Log---------------------------------------" + "\r\n", 'utf8');
+        fs.appendFileSync('log.txt', "\r\n" + "Song Name: " + data.tracks.items[0].name + "\r\n", 'utf8');
+        fs.appendFileSync('log.txt', "\r\n" + "Artist(s): " + data.tracks.items[0].artists[0].name + "\r\n", 'utf8');
+        fs.appendFileSync('log.txt', "\r\n" + "Album: " + data.tracks.items[0].album.name + "\r\n", 'utf8');
+        fs.appendFileSync('log.txt', "\r\n" + "Preview Link: " + data.tracks.items[0].preview_url + "\r\n", 'utf8');
+        fs.appendFileSync('log.txt', "\r\n" + "-------------------------------------------------------" + "\r\n", 'utf8');
+    });
 }
